8192cf3694bd33d3a44057d8d4bf9269c096c51f
galaxyofval <153199753+galaxyofval@users.noreply.github.com>
2025-03-05 14:44:45 -0500

    Update ConsoleApplication4.cpp

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,20 +1,30 @@
+// Program 3
 #include <iostream>
+#include <random>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    float cookies = 0;
-    float sugar = 1.5;
-    float butter = 1;
-    float flour = 2.75;
-    // Ask the user how many cookies they'd like to make.
-    cout << "How many cookies would you like to make? \n";
-    cin >> cookies;
-    // Formulas to translate the default recipe requirements into a different amount of cookies.
-    cout << "You will need: \n" << (cookies * (sugar / 48)) << " cup(s) of sugar. \n";
-    cout << (cookies * (butter / 48)) << " cup(s) of butter. \n";
-    cout << (cookies * (flour / 48)) << " cup(s) of flour. \n";
-
+    double principal = 0;
+    int timesCompounded = 0;
+    double interestRate = 0;
+    cout << fixed << setprecision(2);
+    cout << "Welcome to C++ Interest Calculator! Please answer these few questions below! \n";
+    cout << "How much money do you have in your savings account? \n";
+    cin >> principal;
+    cout << "What is the interest rate of the account? \n";
+    cin >> interestRate;
+    cout << "How many times does said interest compound? \n";
+    cin >> timesCompounded;
+    cout << "Interest Rate: " << interestRate << "%\n";
+    cout << "Times Compounded: " << timesCompounded << endl;
+    cout << "Principal: " << "$" << principal << endl;
+    double interest = pow(1 + (interestRate / timesCompounded), timesCompounded);
+    cout << "Interest: " << "$" << interest << endl;
+    cout << "Amount in Savings: " << "$" << principal + pow(1 + (interestRate / timesCompounded), timesCompounded) << endl;
 
     return 0;
 }
